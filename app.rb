61377ccec9aaa701c4aa185850700e7d3d3284df
Vish Mayer <vish.mayer@gmail.com>
2020-01-07 14:34:53 +0000

    posted params

--- app.rb
+++ app.rb
@@ -19,8 +19,12 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
-  puts params
+post '/named-cat' do
+  p params
   @name = params[:name]
+  erb(:index)
+end
+
+get '/form' do
   erb(:index)
 end
